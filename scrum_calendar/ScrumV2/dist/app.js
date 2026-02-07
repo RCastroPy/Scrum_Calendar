63640b5ba7592d984461162ae1609d4d562a25a3
RCastroPy <castro.rafael2@gmail.com>
2026-02-07 18:50:35 -0300

    Fix data-entry quarterForm reference

--- scrum_calendar/ScrumV2/dist/app.js
+++ scrum_calendar/ScrumV2/dist/app.js
@@ -12350,6 +12350,7 @@
     const celulaForm = qs("#form-celula");
     const personaForm = qs("#form-persona");
     const sprintForm = qs("#form-sprint");
+    const quarterForm = qs("#form-quarter");
     const feriadoForm = qs("#form-feriado");
     const eventoForm = qs("#form-evento");
     const tipoForm = qs("#form-evento-tipo");
